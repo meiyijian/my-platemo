2dc9b07845a4c9d3b7616f0cb162a952db72d0e0
meiyijian <2958776342@qq.com>
2026-09-15 00:15:04 +0800

    删除tmp

--- tmp/weighted_wfg_m15/make_task.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-schtasks /Create /TN WeightedWFG_Resume_M15_M8D10 /XML D:\PlatEMO-master\tmp\weighted_wfg_m15\task_utf16.xml /F
-echo --- query ---
-schtasks /Query /TN WeightedWFG_Resume_M15_M8D10 /V /FO LIST
